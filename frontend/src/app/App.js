a36f16c308fd046e822a42619550ca904d6faebb
yerwqq <139393408+yerwqq@users.noreply.github.com>
2025-08-08 23:18:58 +0300

    Update App.js

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Router from './router';
-import { Toaster } from 'react-hot-toast';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Router />
-      <Toaster />
     </BrowserRouter>
   );
 };
 
 export default App;
+
